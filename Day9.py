3ec72e9983a5abc0fa91933a9fda0d948f030362
ruthcaswellsmith <ruth@plannuh.com>
2024-12-09 16:36:51 -0500

    Day 9 - second try Pt1 is much faster.  Pt2 still very slow.

--- Day9.py
+++ Day9.py
@@ -1,138 +1,108 @@
 from typing import List
 from itertools import groupby
 
-from utils import read_file, Part
+from utils import read_file
 
 
-class DiskMap:
+class DiskMapPt1:
     def __init__(self, data: List[str]):
-        self.map = data[0]
-        self.expanded = []
-        self.compacted = []
-        self.id = 0
+        self.map = [int(ele) for ele in data[0]]
+        self.pos = 0
+        self.checksum = 0
+        self.id_left = 0
+        self.id_right = (len(self.map) - 1) // 2
 
-    @staticmethod
-    def num_free_spaces(input_list: List[str], reverse=False):
-        input_copy = input_list.copy()
-        if reverse:
-            input_copy.reverse()
-        count = 0
-        for item in input_copy:
-            if item == '.':
-                count += 1
-            else:
-                break
-        return count
+    def update_checksum(self, id: int):
+        self.checksum += self.pos * id
+        self.pos += 1
 
-    @staticmethod
-    def num_digits(input_list: List[str], reverse=False):
-        input_copy = input_list.copy()
-        if reverse:
-            input_copy.reverse()
-        count = 0
-        for item in input_copy:
-            if item.isdigit():
-                count += 1
-            else:
-                break
-        return count
+    def compact(self):
+        for i in range(self.map[0]):
+            self.update_checksum(self.id_left)
+        self.map = self.map[1:]
 
-    @staticmethod
-    def count_free_spaces(input_list: List[str]):
-        # only search to the left of the occurrence of file
-        counts, indices = [], []
-        current_index = 0
-        for key, group in groupby(input_list):
-            group_len = len(list(group))
-            if key == ".":
-                counts.append(group_len)
-                indices.append(current_index)
-            current_index += group_len
-        return counts, indices
+        while len(self.map):
+            while self.map[0] > 0 and self.map[-1] > 0:
+                self.map[0] -= 1
+                self.map[-1] -= 1
+                self.update_checksum(self.id_right)
+            if self.map[0] == 0:
+                self.map = self.map[1:]
+                self.id_left += 1
+                for i in range(self.map[0]):
+                    self.update_checksum(self.id_left)
+                self.map = self.map[1:]
+            if self.map and self.map[-1] == 0:
+                self.map = self.map[:-2]
+                self.id_right -= 1
 
-    def expand(self):
-        working = self.map[:]
-        while True:
-            for i in range(int(working[0])):
-                self.expanded.append(str(self.id))
-            self.id += 1
-            if len(working) < 2:
-                break
-            for i in range(int(working[1])):
-                self.expanded.append('.')
-            working = working[2:]
 
-    def compact(self, part: Part):
-        if part == Part.PT1:
-            self.compact_part1()
-        else:
-            self.compact_part2()
-
-    def compact_part1(self):
-        working = self.expanded.copy()
-        while '.' in working:
-            print(len(working))
-            # put beginning in compacted string
-            first_dot = working.index('.')
-            self.compacted += working[:first_dot]
-            working = working[first_dot:]
-            # fill free spaces as much as we can
-            free_spaces_at_beg = self.num_free_spaces(working)
-            numbers_at_end = self.num_digits(working, reverse=True)
-            for i in range(min(free_spaces_at_beg, numbers_at_end)):
-                # move numbers
-                self.compacted.append(working[-1])
-                working = working[:-1]
-                working = working[1:]
-            # Remove trailing free spaces
-            free_spaces_at_end = self.num_free_spaces(working, reverse=True)
-            if free_spaces_at_end > 0:
-                working = working[:-free_spaces_at_end]
-
-        self.compacted += working
-
-    def compact_part2(self):
-        id = (len(self.map) - 1) // 2
-        self.compacted = self.expanded.copy()
-        for id in range(id, 0, -1):
-            # See if we can move this file
-            file_len = int(self.map[id * 2])
-            file = [str(id)] * file_len
-            for file_index, ele in enumerate(self.compacted):
-                if ele == str(id):
-                    break
-
-            counts, indices = self.count_free_spaces(self.compacted[:file_index])
-            for i, count in enumerate(counts):
-                if count >= file_len:
-                    # move this file
-                    # replace the file with free spaces
-                    for j in range(len(self.compacted) - file_len + 1):
-                        if self.compacted[j:j + file_len] == file:
-                            self.compacted[j:j + file_len] = ['.'] * file_len
-                    self.compacted = self.compacted[:indices[i]] + file + \
-                        self.compacted[indices[i] + file_len:]
-                    break
+class DiskMapPt2:
+    def __init__(self, data: List[str]):
+        self.data = data[0]
+        self.max_id = (len(data[0]) - 1) // 2
+        self.map = [('0', int(self.data[0]))]
+        self.data = self.data[1:]
+        for i in range(1, self.max_id + 1):
+            self.map.append(('.', int(self.data[0])))
+            self.map.append((str(i), int(self.data[1])))
+            self.data = self.data[2:]
 
     def get_checksum(self):
         checksum = 0
-        for i in range(len(self.compacted)):
-            if self.compacted[i] != '.':
-                checksum += i * int(self.compacted[i])
+        pos = 0
+        for char, length in self.map:
+            if char != '.':
+                for i in range(length):
+                    checksum += int(char) * pos
+                    pos += 1
+            else:
+                pos += length
         return checksum
+
+    def combine_spaces(self):
+        result = []
+        for char, group in groupby(self.map, key=lambda x: x[0]):
+            total_length = sum(length for _, length in group)
+            result.append((char, total_length))
+        return result
+
+    def compact(self):
+        for i in range(self.max_id, 0, -1):
+            # Find this file
+            for file_pos, (file_char, file_length) in enumerate(self.map):
+                if file_char == str(i):
+                    break
+
+            move_file = False
+            for pos, (char, length) in enumerate(self.map):
+                if pos > file_pos:
+                    # we don't move the file because it's to the right
+                    break
+
+                if char == '.':
+                    if length >= file_length:
+                        move_file = True
+                        break
+
+            if move_file:
+                self.map[pos] = (file_char, file_length)
+                if length > file_length:
+                    self.map = self.map[:pos+1] + [('.', length - file_length)] + self.map[pos+1:]
+                    file_pos += 1
+                self.map[file_pos] = ('.', file_length)
+                self.map = self.combine_spaces()
 
 
 if __name__ == '__main__':
     filename = 'input/Day9.txt'
     data = read_file(filename)
 
-    diskmap = DiskMap(data)
-    diskmap.expand()
-    diskmap.compact(Part.PT1)
-    print(f"The answer to part 1 is {diskmap.get_checksum()}")
+    diskmap = DiskMapPt1(data)
+    diskmap.compact()
+    print(f"The answer to part 1 is {diskmap.checksum}")
 
-    diskmap = DiskMap(data)
-    diskmap.expand()
-    diskmap.compact(Part.PT2)
+    diskmap = DiskMapPt2(data)
+    diskmap.compact()
     print(f"The answer to part 2 is {diskmap.get_checksum()}")
 
