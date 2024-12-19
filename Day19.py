15cebafc4c26b3f348793516c7124afb35d62714
ruthcaswellsmith <ruth@plannuh.com>
2024-12-19 17:30:06 -0500

    Day 19 - Dynamic Programming Approach

--- Day19.py
+++ Day19.py
@@ -1,5 +1,6 @@
 from typing import List
 from functools import lru_cache
+import time
 
 from utils import read_file
 
@@ -17,12 +18,13 @@ class Onsen:
     def answer_pt2(self):
         return sum(self.patterns.values())
 
-    def evaluate(self):
+    def evaluate(self, dp: bool = False):
+        fn = self.count_matches_dp if dp else self.count_matches
         for p in self.patterns.keys():
-            self.patterns[p] = self.find_pattern(p[:])
+            self.patterns[p] = fn(p)
 
     @lru_cache(maxsize=None)
-    def find_pattern(self, p: str):
+    def count_matches(self, p: str):
         if not p:
             return 1
 
@@ -30,9 +32,21 @@ class Onsen:
         for ind in range(1, len(p) + 1):
             for t in self.towels:
                 if p[:ind] == t:
-                    matches += self.find_pattern(p[ind:])
+                    matches += self.count_matches(p[ind:])
 
         return matches
+
+    def count_matches_dp(self, p: str):
+        dp = [0] * (len(p) + 1)
+        dp[0] = 1
+
+        for i in range(1, len(p) + 1):
+            for j in range(i):
+                prefix = p[j:i]
+                if prefix in self.towels:
+                    dp[i] += dp[j]
+
+        return dp[len(p)]
 
 
 if __name__ == '__main__':
@@ -40,7 +54,14 @@ if __name__ == '__main__':
     data = read_file(filename)
 
     onsen = Onsen(data[0], data[2:])
+    start = time.time()
     onsen.evaluate()
-    print(f"The answer to part 1 is {onsen.answer_pt1}")
+    print(f"Using recursion took {time.time() - start} sec")
 
+    start = time.time()
+    onsen.evaluate(dp=True)
+    print(f"Using DP took {time.time() - start} sec")
+
+    print(f"The answer to part 1 is {onsen.answer_pt1}")
     print(f"The answer to part 2 is {onsen.answer_pt2}")
+
