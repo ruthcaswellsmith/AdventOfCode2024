c4a99153a5aad71a21fc6c021b74cb271200e2ca
ruthcaswellsmith <ruth@plannuh.com>
2024-12-26 07:00:31 -0500

    Day 24

--- Day24.py
+++ Day24.py
@@ -1,5 +1,6 @@
-from typing import List
 import operator
+from typing import List
+
 from utils import read_file
 
 OPS = {'AND': operator.and_, 'OR': operator.or_, 'XOR': operator.xor}
@@ -7,11 +8,17 @@ OPS = {'AND': operator.and_, 'OR': operator.or_, 'XOR': operator.xor}
 
 class Device:
     def __init__(self, data: List[str]):
-        index = data.index('')
-        self.gates = self.get_gates(data[index+1:])
-        self.wires = self.get_wires(data[:index])
+        self.gates = self.get_gates(data[data.index('') + 1:])
+        self.wires = None
         self.max_z = max([int(w[1:]) for w in self.gates.keys() if w.startswith('z')])
         self.swaps = []
+        self.x, self.y, self.z = self.format_wire('x', 0), self.format_wire('y', 0), self.format_wire('z', 0)
+        self.x_and_y = self.get_gate(self.x, "AND", self.y)
+        self.x_xor_y = self.get_gate(self.x, "XOR", self.y)
+        # This is true because the carry bit is 0 for the 0th bit
+        self.x_xor_y_and_c = 0
+        self.sum = self.x_xor_y
+        self.carry = self.x_and_y
 
     @staticmethod
     def format_wire(letter: str, i: int):
@@ -32,12 +39,11 @@ class Device:
                       w in sorted([wire for wire in self.wires.keys() if wire.startswith(letter)], reverse=True))
         return int(num, 2)
 
-    def get_wires(self, data: List[str]):
-        wires = {w: None for w in self.gates.keys()}
+    def initialize(self, data: List[str]):
+        self.wires = {w: None for w in self.gates.keys()}
         for line in data:
             pts = line.replace(' ', '').split(':')
-            wires[pts[0]] = int(pts[1])
-        return wires
+            self.wires[pts[0]] = int(pts[1])
 
     @staticmethod
     def get_gates(data: List[str]):
@@ -59,40 +65,43 @@ class Device:
                 if self.wires[w1] is not None and self.wires[w2] is not None:
                     self.wires[output] = OPS[op](self.wires[w1], self.wires[w2])
 
-    def fix_gates(self):
-        # Thanks to _garden_gnome_ whose post helped me understand how an adder works
-        x, y = self.format_wire('x', 0), self.format_wire('y', 0)
+    def update_state(self, carry_prev: str):
+        self.x_and_y = self.get_gate(self.x, 'AND', self.y)
+        self.x_xor_y = self.get_gate(self.x, "XOR", self.y)
+        self.x_xor_y_and_c = self.get_gate(self.x_xor_y, "AND", carry_prev)
+        self.sum = self.get_gate(self.x_xor_y, "XOR", carry_prev)
+        self.carry = self.get_gate(self.x_xor_y_and_c, "OR", self.x_and_y)
 
-        gate_carry_prev = self.get_gate(x, "AND", y)
+    def fix_gates(self):
 
         for i in range(1, self.max_z):
-            x, y, z = self.format_wire('x', i), self.format_wire('y', i), self.format_wire('z', i)
+            carry_prev = self.carry
+            self.x, self.y, self.z = self.format_wire('x', i), self.format_wire('y', i), self.format_wire('z', i)
+            self.update_state(carry_prev)
+            w1, op, w2 = self.gates[self.z]
 
-            still_swapping = True
-            while still_swapping:
-                still_swapping = False
-
-                gate_and = self.get_gate(x, 'AND', y)
-                gate_xor = self.get_gate(x, "XOR", y)
-                w1, op, w2 = self.gates[z]
-
-                if w1 == gate_carry_prev and w2 != gate_xor:
-                    self.swap(w2, gate_xor)
-                    still_swapping = True
-                    continue
-                if w2 == gate_carry_prev and w1 != gate_xor:
-                    self.swap(w1, gate_xor)
-                    still_swapping = True
-                    continue
-
-                gate_z = self.get_gate(gate_xor, "XOR", gate_carry_prev)
-                if gate_z != z:
-                    self.swap(gate_z, z)
-                    still_swapping = True
-                    continue
-
-                gate_tmp = self.get_gate(gate_xor, "AND", gate_carry_prev)
-                gate_carry_prev = self.get_gate(gate_tmp, "OR", gate_and)
+            if op == 'XOR':
+                if (w1, w2) in ((carry_prev, self.x_xor_y), (self.x_xor_y, carry_prev)):
+                    pass
+                elif w1 == carry_prev or w2 == carry_prev:
+                    if w1 == carry_prev:
+                        self.swap(w2, self.x_xor_y)
+                        self.update_state(carry_prev)
+                    elif w2 == carry_prev:
+                        self.swap(w1, self.x_xor_y)
+                        self.update_state(carry_prev)
+                    else:
+                        print(f"Can't fix bit {i}");
+                        exit()
+            else:
+                if (w1, w2) in ((carry_prev, self.x_xor_y), (self.x_xor_y, carry_prev),
+                                (self.x, self.y), (self.y, self.x),
+                                (self.x_xor_y_and_c, self.x_and_y), (self.x_and_y, self.x_xor_y_and_c)):
+                    self.swap(self.sum, self.z)
+                    self.update_state(carry_prev)
+                else:
+                    print(f"Can't fix bit {i}");
+                    exit()
 
 
 if __name__ == '__main__':
@@ -100,9 +109,14 @@ if __name__ == '__main__':
     data = read_file(filename)
 
     device = Device(data)
+    device.initialize(data[:data.index("")])
     device.simulate()
     print(f"The answer to part 1 is {device.number('z')}.")
 
     device.fix_gates()
     print(f"The answer to part 2 is {','.join(sorted(device.swaps))}.")
 
+    device.initialize(data[:data.index("")])
+    device.simulate()
+    print(f"{device.number('x')} + {device.number('y')} ?= {device.number('z')}")
+    print(f"{device.number('x') + device.number('y')}")
